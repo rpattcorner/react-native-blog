1e0b137501567f15920868d7861f08738e5d0674
rpattcorner <robert.patt-corner@eglobaltech.com>
2020-10-23 13:30:37 -0400

    fixed - blogPosts is data structure, not function

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,10 +3,7 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ( {children}  ) => {
-    const BlogPosts = () => {
-        { title: 'Blog Post #1' }
-        { title: 'Blog Post #2a '}
-    }
+    const BlogPosts = [{ title: 'Blog Post #1' },{ title: 'Blog Post #2a '}];
     return <BlogContext.Provider value = {BlogPosts}>
         {children}
     </BlogContext.Provider>;
