861dbe9b84290dadb5be22ab96cbfb217de2e87d
rpattcorner <robert.patt-corner@eglobaltech.com>
2020-10-23 14:05:42 -0400

    working with useState

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ( {children}  ) => {
-    const BlogPosts = [{ title: 'Blog Post #1' },{ title: 'Blog Post #2a '}];
-    return <BlogContext.Provider value = {BlogPosts}>
+    const [blogPosts, setBlogPosts] = useState([]);
+
+    const addBlogPost = () => {
+        setBlogPosts( [
+             ...blogPosts, { title: `Blog Post # ${blogPosts.length + 1}`}
+        ]);
+    }
+
+    return <BlogContext.Provider value = { {data: blogPosts, addBlogPost: addBlogPost}}>
         {children}
     </BlogContext.Provider>;
 }
 
-export default BlogContext;
+export default BlogContext; 
